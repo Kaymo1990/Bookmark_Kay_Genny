502cda69c2246e5fc0a5d292f942d6b46091e82d
Khurram Mohammed <khurrammohammed1990@gmail.com>
2019-07-19 14:09:00 +0100

    step 14

---
+++ app.rb
@@ -0,0 +1,51 @@
+
+require 'sinatra/base'
+require './lib/bookmark'
+
+class BookmarkManager < Sinatra::Base
+  get '/' do
+    @bookmarks = Bookmark.all
+    erb :index
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :index
+  end
+
+  get '/bookmarks/new' do
+    erb :"bookmarks/new"
+  end
+
+
+  post '/bookmarks' do
+    Bookmark.create(url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
+    enable :sessions, :method_override
+
+  delete '/bookmarks/:id' do
+  Bookmark.delete(id: params[:id])
+  redirect '/bookmarks'
+end
+
+# get '/bookmarks/:id/edit' do
+#   @bookmark_id = params[:id]
+#   erb :'bookmarks/edit'
+# end
+
+patch '/bookmarks/:id' do
+  Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+  redirect '/bookmarks'
+end
+
+get '/bookmarks/:id/edit' do
+  @bookmark = Bookmark.find(id: params[:id])
+  erb :"bookmarks/edit"
+end
+
+
+
+  run! if app_file == $0
+end
